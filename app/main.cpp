038e746d30f9ad60990318fd17278d5cd471ea79
Benoît Rouits <brouits@free.fr>
2021-05-17 02:48:52 +0200

    Fix usage of scalable icon

--- app/main.cpp
+++ app/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 	AbcMainWindow w;
 	abcapplication.setMainWindow(&w);
 
-    QString iconpath = QString(DATADIR "/icons/hicolor/128x128/apps/" TARGET ".png");
+    QString iconpath = QString(DATADIR "/icons/hicolor/scalable/apps/" TARGET ".svg");
 	if (QFileInfo::exists(iconpath))
 		abcapplication.setWindowIcon(QIcon(iconpath));
 

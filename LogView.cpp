e595d25cb3718fbea4bea275438a8be094ca9e70
Benoît Rouits <brouits@free.fr>
2021-11-23 03:54:32 +0100

    Add copyright notice in source files

--- LogView.cpp
+++ LogView.cpp
@@ -1,3 +1,13 @@
+// This source code is part of QAbc, a minimal ABC music notation editor.
+// QAbc is Copyright © 2021 Benoît Rouits <brouits@free.fr>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "LogView.h"
 
 LogView::LogView(QWidget* parent)
